5e6d63746ee339aeff4a2d433d9598084bfdf2b6
PawelPruchniak <pw.pruchniak@gmail.com>
2022-08-04 23:15:06 +0200

    Added tests for savings security service

--- security/src/test/java/pp/pl/io/savings/core/SavingsSecurityServiceImplementationTest.java
+++ security/src/test/java/pp/pl/io/savings/core/SavingsSecurityServiceImplementationTest.java
@@ -1,5 +1,6 @@
 package pp.pl.io.savings.core;
 
+import io.vavr.collection.HashSet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -10,6 +11,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import pp.pl.io.savings.organisation.SavingsSecurityService;
+import pp.pl.io.savings.organisation.UserRole;
+import pp.pl.io.savings.organisation.UserWithRoles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -23,7 +26,7 @@ class SavingsSecurityServiceImplementationTest {
   private static final UserDetails USER_DETAILS = User.builder()
       .username("some-user-email@gmail.com")
       .password("some-password")
-      .authorities("USER_ROLE")
+      .authorities("ROLE_1", "ROLE_2")
       .build();
 
   private static final UsernamePasswordAuthenticationToken USER_TOKEN =
@@ -52,5 +55,47 @@ class SavingsSecurityServiceImplementationTest {
     var username = savingsSecurityService.getUsername();
 
     assertEquals(USER_DETAILS.getUsername(), username);
+  }
+
+  @Test
+  void shouldReturnEmptyRolesWhenMissingAuthentication() {
+    SecurityContextHolder.setContext(securityContext);
+    when(securityContext.getAuthentication())
+        .thenReturn(null);
+
+    var roles = savingsSecurityService.getUserRoles();
+
+    assertEquals(HashSet.empty(), roles);
+  }
+
+  @Test
+  void shouldReturnRolesSuccessfully() {
+    SecurityContextHolder.setContext(securityContext);
+    when(securityContext.getAuthentication())
+        .thenReturn(USER_TOKEN);
+
+    var roles = savingsSecurityService.getUserRoles();
+
+    assertEquals(
+        HashSet.of(UserRole.of("ROLE_1"), UserRole.of("ROLE_2")),
+        roles
+    );
+  }
+
+  @Test
+  void shouldReturnUserWithRolesSuccessfully() {
+    SecurityContextHolder.setContext(securityContext);
+    when(securityContext.getAuthentication())
+        .thenReturn(USER_TOKEN);
+
+    var userWithRoles = savingsSecurityService.getUserWithRoles();
+
+    assertEquals(
+        UserWithRoles.builder()
+            .username(USER_DETAILS.getUsername())
+            .roles(HashSet.of(UserRole.of("ROLE_1"), UserRole.of("ROLE_2")))
+            .build(),
+        userWithRoles
+    );
   }
 }
